5298bce5b81964e40be32b8f2f80064aa7f7bcbd
BrunoSapalacio <bruno2014mineiro@gmail.com>
2022-10-26 22:44:55 -0300

    Feat: Animation Home

--- src/components/home.js
+++ src/components/home.js
@@ -2,23 +2,41 @@ import React from 'react'
 
 import 'animate.css'
 import '../styles/home.scss'
+import '../styles/background.scss'
 
 import Code from '../images/code-typing-bro.svg'
 
 const Home = () => {
   return (
     <section className='home animate__animated animate__slideInLeft'>
+      <div className='lines'>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
       <div className='title-box'>
         <div>
           <div className='msg-box'>
           <p>Olá, meu nome é</p>
           </div>
-          <h1>Bruno</h1>
-          <h1>Sapalacio</h1>
+          <div className="stack" style={{'--stacks': "3"}}>
+            <h1 style={{'--index': "0"}}>BRUNO</h1>
+            <h1 style={{'--index': "1"}}>BRUNO</h1>
+            <h1 style={{'--index': "2"}}>BRUNO</h1>
+          </div>
+          <div className="stack" style={{'--stacks': "3"}}>
+            <h1 style={{'--index': "0"}}>SAPALACIO</h1>
+            <h1 style={{'--index': "1"}}>SAPALACIO</h1>
+            <h1 style={{'--index': "2"}}>SAPALACIO</h1>
+          </div>
           <h3>Desenvolvedor Frontend | HTML | CSS | JavaScript | React | Sass | Bootstrap</h3>
           <ul>
-            <li><a href='https://github.com/BrunoSapalacio' target="_blank" rel="noreferrer"><i className="devicon-github-original colored tooltip"><span className="tooltiptext">Clique aqui para ver o meu Github</span></i></a></li>
-            <li><a href='https://www.linkedin.com/in/bruno-sapalacio-mineiro-0b7a25231/' target="_blank" rel="noreferrer"><i className="devicon-linkedin-plain colored tooltip"><span className="tooltiptext">Clique aqui para ver o meu LinkedIn</span></i></a></li>
+          <a className="github-2 tooltip" href='https://github.com/BrunoSapalacio' target="_blank" rel="noreferrer"><span className="tooltiptext">GitHub</span></a>
+          <a className="linkedin tooltip" href='https://www.linkedin.com/in/bruno-sapalacio-mineiro-0b7a25231/' target="_blank" rel="noreferrer"><span className="tooltiptext">LinkedIn</span></a>
+          <a className="instagram tooltip" href='https://www.instagram.com/bruno.sapalacio/?hl=pt-br' target="_blank" rel="noreferrer"><span className="tooltiptext">Instagram</span></a>
+          <a className="facebook tooltip" href='https://www.facebook.com/bruno.sapalacio' target="_blank" rel="noreferrer"><span className="tooltiptext">Facebook</span></a>
           </ul>
         </div>
       </div>
